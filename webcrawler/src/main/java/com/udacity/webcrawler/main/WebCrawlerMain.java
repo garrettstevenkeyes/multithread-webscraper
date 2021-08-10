a7ff37e8d2d8c33932aab531470452df180cef29
“Garrett <44751888+garrettstevenkeyes@users.noreply.github.com>
2021-08-09 22:32:36 -0400

    main changes

--- webcrawler/src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
+++ webcrawler/src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
@@ -36,14 +36,14 @@ public final class WebCrawlerMain {
     CrawlResult result = crawler.crawl(config.getStartPages());
     CrawlResultWriter resultWriter = new CrawlResultWriter(result);
     // TODO: Write the crawl results to a JSON file (or System.out if the file name is empty)
-//    try {
-//      Writer writer = new FileWriter("path/to/file.json");
-//      resultWriter.write(writer);
-//    } catch (IOException exception) {
+    try {
+      Writer writer = new FileWriter(config.toString());
+      resultWriter.write(writer);
+    } catch (IOException exception) {
 //    // TODO: Write the profile data to a text file (or System.out if the file name is empty)
-//      Writer writer = new FileWriter("path/to/file.txt");
-//      resultWriter.write(writer);
-//    }
+      Writer writer = new FileWriter("path/to/file.txt");
+      resultWriter.write(writer);
+    }
 
 
   }
